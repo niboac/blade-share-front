8f5e63c628d8a32523456bbcb0c126f474d02f10
caobin <binc@hupofintech.com>
2017-12-15 14:05:32 +0800

    overallEvaluationPrice: '综合评估价',

--- dist/options.js
+++ dist/options.js
@@ -282,12 +282,15 @@ var hlMortgageLabel = exports.hlMortgageLabel = {
   housingAgency2: '房屋中介名称2',
   agencyEvaluationPrice1: '中介评估价1',
   agencyEvaluationPrice2: '中介评估价2',
+  overallEvaluationPrice: '综合评估价',
   fastBid1: '快出价1',
   fastBid2: '快出价2',
   houseOwner: '产权人',
   estateResearch: '产调情况',
   mortgageRemain: '抵押余额',
-  originalLoanType: '原贷款类型'
+  originalLoanType: '原贷款类型',
+  firstMortgageRemain: '一抵余额',
+  secondMortgageRemain: '二抵余额'
 };
 
 var orderLabel = exports.orderLabel = {
