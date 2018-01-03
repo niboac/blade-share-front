9c0194ffcafcf101f69eeaa584534dfec94a2888
杨杉 <shany@hupofintech.com>
2018-01-03 16:27:23 +0800

    unionBorrower

--- dist/options.js
+++ dist/options.js
@@ -897,7 +897,7 @@ var blockSetting = {
       共借人信息: {
         iconKey: '#icon-kehuziliao',
         routeName: '补充附加信息',
-        objName: 'unionBorrower'
+        objName: 'unionBorrowerNoReq'
       },
       抵押物信息: {
         iconKey: '#icon-kehuziliao',
