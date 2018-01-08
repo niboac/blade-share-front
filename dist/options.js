a4bdbe80c288803fbb3538009ba41f5be6259bbc
杨杉 <shany@hupofintech.com>
2018-01-08 18:38:50 +0800

    no message

--- dist/options.js
+++ dist/options.js
@@ -936,7 +936,7 @@ var blockSetting = {
     },
     下户: Object.assign(xiahuInCommon, {
       补充抵押资料: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         routeName: '补充信息',
         objName: 'hlMortgage'
       },
@@ -947,13 +947,13 @@ var blockSetting = {
     }),
     合同: {
       公证: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '公证', group: 'notarize' }]
       }
     },
     抵押: {
       抵押: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '抵押证', group: 'diya' }, { label: '抵押受理通知单/收据', group: 'shouju' }]
       }
     }
@@ -962,17 +962,17 @@ var blockSetting = {
   SHU_LOU_DAI: {
     下户: Object.assign(xiahuInCommon, {
       下户照片: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '定位截图', group: '定位截图' }, { label: '小区大门', group: '小区大门', imageRequire: '小区大门必须体现小区名，如无小区名需拍摄附近地标建筑（银行等）' }, { label: '楼号', group: '楼号' }, { label: '单元号', group: '单元号' }, { label: '门牌号', group: '门牌号' }, { label: '客厅', group: '客厅' }, { label: '卧室', group: '卧室', imageRequire: '每室不少于一张' }, { label: '厨房', group: '厨房' }, { label: '卫生间', group: '卫生间' }, { label: '阳台', group: '阳台' }, { label: '阳台外拍', group: '阳台外拍', imageRequire: '左中右合计3张' }, { label: '小区内景', group: '小区内景', imageRequire: '3张以上' }, { label: '居住细节', group: '居住细节', imageRequire: '即家庭合影、婚纱照、书桌、卫生间清洁度等细节类照片' }, { label: '户型图', group: '户型图', imageRequire: '接受网查截图或手绘草图' }, { label: '合影', group: '合影' }]
       }
     }, xaihuSld),
     核行: {
       核行资料: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '核行定位截图', group: '核行定位截图' }, { label: '核行地合影', group: '核行地合影' }, { label: '问询记录名片', group: '问询记录名片' }, { label: '其他核行证明材料', group: '其他核行证明材料' }]
       },
       核行确认表: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '核行确认表', group: '核行确认表' }],
         hideBtn: true
       }
@@ -980,73 +980,73 @@ var blockSetting = {
     },
     合同: {
       借款合同: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '借款合同', group: '借款合同', imageRequire: '最多10张，单张不超过2MB' }]
       },
       信用咨询及管理服务协议: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '信用咨询及管理服务协议', group: '信用咨询及管理服务协议', imageRequire: '最多10张，单张不超过2MB' }]
       },
       划款委托及承诺书: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '划款委托及承诺书', group: '划款委托及承诺书', imageRequire: '最多10张，单张不超过2MB' }]
       },
       代还款委托书: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '代还款委托书', group: '代还款委托书', imageRequire: '最多10张，单张不超过2MB' }]
       },
       借据: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '借据', group: '借据', imageRequire: '最多10张，单张不超过2MB' }]
       },
       租赁合同: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '租赁合同', group: '租赁合同', imageRequire: '最多10张，单张不超过2MB' }]
       },
       购销合同: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '购销合同', group: '购销合同', imageRequire: '最多10张，单张不超过2MB' }]
       },
       同意借款声明书: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '同意借款声明书', group: '同意借款声明书', imageRequire: '最多10张，单张不超过2MB' }]
       },
       业务申请表: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '业务申请表', group: '业务申请表', imageRequire: '最多10张，单张不超过2MB' }]
       },
       面签拍照: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '面签拍照', group: '面签拍照', imageRequire: '最多10张，单张不超过2MB' }]
       }
     },
     控件: {
       控件照片: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '控件照片', group: '控件照片', imageRequire: '最多10张，单张不超过2MB' }]
       }
     },
     归档: {
       归档照片: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '归档照片', group: '归档照片', imageRequire: '最多10张，单张不超过2MB' }]
       }
     },
     解抵押: {
       '抵押权注销凭单、章': {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '抵押权注销凭单、章', group: '抵押权注销凭单、章', imageRequire: '最多10张，单张不超过2MB' }]
       }
     },
     进抵: {
       抵押证照片: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '抵押证照片', group: '抵押证照片', imageRequire: '最多10张，单张不超过2MB' }]
       }
     },
     归档结案: {
       结案照片: {
-        iconKey: '#icon-kehuziliao',
+        iconKey: 'icon-zhaopian',
         imgUrls: [{ label: '结案照片', group: '结案照片', imageRequire: '最多10张，单张不超过2MB' }]
       }
     }
