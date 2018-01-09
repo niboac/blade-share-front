b954aaf91afb3f1e88f428c692607d684fd4c9ae
杨杉 <shany@hupofintech.com>
2018-01-09 10:15:47 +0800

    下户抵押照片

--- src/img-group-setting.js
+++ src/img-group-setting.js
@@ -106,7 +106,7 @@ const blockSetting = {
     },
     下户: Object.assign(xiahuInCommon, {
       补充抵押资料: {
-        iconKey: 'icon-zhaopian',
+        iconKey: '#icon-kehuziliao',
         routeName: '补充信息',
         objName: 'hlMortgage',
       },
