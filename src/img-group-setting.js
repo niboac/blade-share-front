42cb1a5a3f5a4ecba148b984a60208adfead35ab
caobin <binc@hupofintech.com>
2018-01-09 17:50:06 +0800

    no message

--- src/img-group-setting.js
+++ src/img-group-setting.js
@@ -88,20 +88,20 @@ const blockSetting = {
       上传图片: {
         iconKey: 'icon-zhaopian',
         imgUrls: [
-          { label: '婚姻证明', group: 'marriage' },
-          { label: '户口本', group: 'household' },
-          { label: '房产证', group: '房产证' },
-          { label: '其他资料', group: 'other' },
+          { label: '婚姻证明', group: 'marriage', limit: 10, uploadType: 'multiPic' },
+          { label: '户口本', group: 'household' , limit: 10, uploadType: 'multiPic'},
+          { label: '房产证', group: '房产证' , limit: 10, uploadType: 'multiPic'},
+          { label: '其他资料', group: 'other' , limit: 10, uploadType: 'multiPic'},
         ],
         backRoutePath: '/nd/buchong'
       },
       进件上传图片: {
         iconKey: 'icon-zhaopian',
         imgUrls: [
-          { label: '婚姻证明', group: 'marriage' },
-          { label: '户口本', group: 'household' },
-          { label: '房产证', group: '房产证' },
-          { label: '其他资料', group: 'other' },
+          { label: '婚姻证明', group: 'marriage', limit: 10, uploadType: 'multiPic' },
+          { label: '户口本', group: 'household', limit: 10, uploadType: 'multiPic' },
+          { label: '房产证', group: '房产证' , limit: 10, uploadType: 'multiPic'},
+          { label: '其他资料', group: 'other' , limit: 10, uploadType: 'multiPic'},
         ],
         backRoutePath: '/nd/index'
       },
