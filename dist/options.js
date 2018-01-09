75b8759f0875d27708298c6fe6563863003f28c6
杨杉 <shany@hupofintech.com>
2018-01-09 14:38:00 +0800

    no message

--- dist/options.js
+++ dist/options.js
@@ -925,7 +925,7 @@ var blockSetting = {
       },
       上传图片: {
         iconKey: 'icon-zhaopian',
-        imgUrls: [{ label: '婚姻证明', group: '婚姻证明' }, { label: '户口本', group: '户口本' }, { label: '房产证其他照片', group: '房产证其他照片' }],
+        imgUrls: [{ label: '婚姻证明', group: '婚姻证明' }, { label: '户口本', group: '户口本' }, { label: '房产证其他照片', group: '房产证其他照片' }, { label: '其他资料', group: '其他资料' }],
         backRoutePath: '/nd/buchong'
       },
       进件上传图片: {
