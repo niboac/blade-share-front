8ec4792fd47a7d6529fb1364efb4e3000d1e600c
杨杉 <shany@hupofintech.com>
2018-01-18 16:06:26 +0800

    test

--- src/options.js
+++ src/options.js
@@ -64,7 +64,7 @@ export const applicantLabel = {
   professionalTitle: '职称',
   title: '职务',
   yearOfWork: '工作年限',
-  annualIncome: '个人年收入',
+
   annualIncomeRemarks: null,
   graduatedSchool: '毕业学校',
   educationLevel: '最高学历',
@@ -104,6 +104,7 @@ export const applicantLabel = {
   birthdate: '出生日期',
   // clientWorkType: null,
   extra: {
+    annualIncome: '个人年收入',
     clientLoanType: null,
     clientWorkType: '客户类型',
     addressType: '通讯地址类型',
